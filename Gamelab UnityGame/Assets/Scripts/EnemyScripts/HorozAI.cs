19a7d2fd1f7f4f5d7b525a274c76f15f64c091a9
Rogedeen <ynsisikgoz@gmail.com>
2025-12-04 10:35:54 +0300

    level design 0

--- Gamelab UnityGame/Assets/Scripts/EnemyScripts/HorozAI.cs
+++ Gamelab UnityGame/Assets/Scripts/EnemyScripts/HorozAI.cs
@@ -148,11 +148,14 @@ public EnemyType enemyType;
 
     void LamayiAra()
     {
-        Collider2D arkaHit = Physics2D.OverlapCircle(transform.position, duymaMesafesi, lamaLayer);
-
-        if (arkaHit != null) {
-            TespitEt(arkaHit.transform);
-            return;
+        Collider2D[] yakinHedefler = Physics2D.OverlapCircleAll(transform.position, duymaMesafesi, lamaLayer);
+        foreach (Collider2D hedef in yakinHedefler)
+        {
+            if (hedef.CompareTag("Player"))
+            {
+                TespitEt(hedef.transform);
+                return; 
+            }
         }
 
 
@@ -162,7 +165,15 @@ public EnemyType enemyType;
 
         if (hit.collider != null)
         {
-            TespitEt(hit.transform);
+            if (hit.collider.CompareTag("Player"))
+            {
+                // Evet, oyuncuyu gördük!
+                TespitEt(hit.transform);
+            }
+            else
+            {
+
+            }
         }
     }
 
